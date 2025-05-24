3f02dc257e1ab3156c2f19e0087795a5d88925c3
Raghad <raghadmoslli222@gmail.com>
2025-05-25 00:10:14 +0300

    post to get

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -90,20 +90,26 @@ public function addStudent(StoreStudentRequest $request)
 
 
 //________________________________________________________________________________________
-public function getStudentsByGradeAndClassroom(Request $request)
+
+public function getStudentsByGradeAndClassroom($gradeName, $sectionName)
 {
-    $validated = $request->validate([
-        'grade' => 'required|string',
-        'section' => 'required|string',
-    ]);
+    $grade = Grade::where('name', $gradeName)->first();
 
-
-    $grade = Grade::where('name', $validated['grade'])->first();
-
+    if (!$grade) {
+        return response()->json([
+            'message' => 'الصف غير موجود.',
+        ], 404);
+    }
 
     $classroom = Classroom::where('grade_id', $grade->id)
-        ->where('name', $validated['section'])
+        ->where('name', $sectionName)
         ->first();
+
+    if (!$classroom) {
+        return response()->json([
+            'message' => 'الشعبة غير موجودة لهذا الصف.',
+        ], 404);
+    }
 
     $students = Student::where('classroom_id', $classroom->id)
         ->with('user')
--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,7 @@ Route::post('addNote', [NoteController::class, 'storeNote']);
 Route::post('updateNote/{id}', [NoteController::class, 'update']);
 Route::delete('deleteNote/{id}', [NoteController::class, 'destroy']);
 Route::get('allnoteStudent', [NoteController::class, 'allnoteStudent'])->middleware('auth:sanctum');
-Route::post('getStudentsByGradeAndClassroom', [StudentController::class, 'getStudentsByGradeAndClassroom']);
+Route::get('getStudentsByGradeAndClassroom/{gradeName}/{sectionName}', [StudentController::class, 'getStudentsByGradeAndClassroom']);
 Route::post('storeWeeklySchedule', [WeeklyScheduleController::class, 'storeWeeklySchedule']);
 Route::get('getWeeklySchedule', [WeeklyScheduleController::class, 'getWeeklySchedule'])->middleware('auth:sanctum');
 Route::get('getClassroomsByGrade/{gradeName}', [ClassroomController::class, 'getClassroomsByGrade']);
