131c96fe4b0c1885c99355012508487c73c7a70c
Raghad <raghadmoslli222@gmail.com>
2025-05-24 23:59:19 +0300

    ..

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -8,29 +8,6 @@ use App\Models\Classroom;
 
 class ClassroomController extends Controller
 {
-//     public function getClassroomsByGrade(Request $request)
-// {
-
-//     $validated = $request->validate(['grade' => 'required|string']
-//     , [
-//         'grade.required' => 'الرجاء إدخال اسم الصف.',
-//     ]);
-
-
-//     $grade = Grade::where('name', $validated['grade'])->first();
-
-//     if (!$grade) {
-//         return response()->json([
-//             'message' => 'الصف غير موجود.',
-//         ], 404);
-//     }
-
-
-//     $classrooms = Classroom::where('grade_id', $grade->id)->get();
-
-//     return response()->json(['grade' => $validated['grade'], 'classrooms' => $classrooms ]);
-// }
-
 
 public function getClassroomsByGrade($gradeName)
 {
