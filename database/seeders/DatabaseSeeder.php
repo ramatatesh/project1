0ef3125838feb10524ae141aa1c40332d5d5ec70
ramatatesh <www.ramatatesh40@gmail.com>
2025-05-30 21:48:49 +0530

    صص

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             GradeSeeder::class,
-            ClassroomSeeder::class ]);
+            ClassroomSeeder::class,
+            SubjectSeeder::class,]);
     }
 }
